fd4000184e6946c7623d09b351ffe22d902e96c3
RitikBT <88572047+RitikBT@users.noreply.github.com>
2021-10-18 09:40:41 +0530

    Create New Method

--- MyJava.java
+++ MyJava.java
@@ -2,8 +2,12 @@ class MyJava{
 public static void main(String[] args){
 
 System.out.println("Only for practise");
-System.out.println("Now I am going to cone this repo and also add some  more file");
+System.out.println("Now I am going to clone this repo and also add some  more line of codes");
 
 }
+  
+  public static void evenOddNumber(){
+  
+  }
 
 }
