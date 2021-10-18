ab9d4705d344ebb293c6d2dd7e9bcc239dd7d40b
RitikBT <88572047+RitikBT@users.noreply.github.com>
2021-10-18 09:38:14 +0530

    Create MyJava.java

---
+++ MyJava.java
@@ -0,0 +1,9 @@
+class MyJava{
+public static void main(String[] args){
+
+System.out.println("Only for practise");
+System.out.println("Now I am going to cone this repo and also add some  more file");
+
+}
+
+}
