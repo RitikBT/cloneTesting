d24d4790cd87116ed3354b7e8f934f7629869da7
RitikBT <88572047+RitikBT@users.noreply.github.com>
2021-10-18 11:42:29 +0530

    Add files via upload
    New Project Added

---
+++ RitikGit Project/src/MyJava.java
@@ -0,0 +1,24 @@
+class MyJava{
+	public static void main(String[] args){
+
+		System.out.println("Only for practise");
+
+		evenOddNumber(15);
+	}
+
+	public static void evenOddNumber(int num){
+		if(num%2==0)
+			System.out.println("Number is Even");
+		else
+			System.out.println("Number is Odd");
+	}
+
+	public static void withoutModEvenOdd(int num){
+		int temp=num/2;
+		if(temp*2==num)
+			System.out.println("Number is Even");
+		else
+			System.out.println("Number is Odd");
+	}
+
+}
