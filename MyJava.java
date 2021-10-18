e2cc0b4f61d13b34a741e581be038420f410d4aa
Ritik <ritikbt2001@gmail.com>
2021-10-18 09:55:56 +0530

    Some Metho Adeed.......

--- MyJava.java
+++ MyJava.java
@@ -2,12 +2,23 @@ class MyJava{
 public static void main(String[] args){
 
 System.out.println("Only for practise");
-System.out.println("Now I am going to clone this repo and also add some  more line of codes");
 
+evenOddNumber(15);
 }
   
-  public static void evenOddNumber(){
-  
+  public static void evenOddNumber(int num){
+if(num%2==0)
+    System.out.println("Number is Even");
+else
+   System.out.println("Number is Odd");
   }
+
+public stattic void withoutModEvenOdd(int num){
+int temp=num/2;
+if(temp*2==num)
+     System.out.println("Number is Even");
+else
+     System.out.println("Number is Odd");
+}
 
 }
